9666d08843e6b583f80bdada21f439768b1d7485
林帆 <flin@thoughtworks.com>
2017-01-15 15:21:00 +0800

    Use v2 image

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ redis = Redis(host= "redis", port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'You have hit this page {0} times. - Edition v1' . format (redis.get( 'hits' ))
+    return 'You have hit this page {0} times. - Edition v2' . format (redis.get( 'hits' ))
 
 if __name__ == "__main__":
     app.run(host= "0.0.0.0", debug=False)
